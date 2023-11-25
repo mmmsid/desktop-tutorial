3a6b03277a609ceda849587f3e0f54d3b6a40c1b
Maria <mmm.sid2@icloud.com>
2023-11-25 16:19:56 +0300

    new lesson

---
+++ script.js
@@ -0,0 +1,24 @@
+const a = 'Привет, мир!';
+const b = 5;
+const c = 7;
+
+let name = '';
+
+let input = document.getElementById('myInput').value;
+let inpetValue = input.value;
+
+function info(string) {
+    alert(`Hello World! ${string} ${name} ${inputValue}`);
+}
+
+function inputAlert(){
+alert(document.getElementById('myInput').value);
+}
+
+function toggleName(string){
+name = string
+}
+console.log(b+c);
+
+
+
